c541273c7af711b8ebbcd09b66bf279a5ebe981b
AltafMShaikh <75683704+AltafMShaikh@users.noreply.github.com>
2021-01-15 19:58:31 +0530

    Update Loop.js

--- Task 2/Loop.js
+++ Task 2/Loop.js
@@ -6,7 +6,7 @@
     <title>Task 2</title>
   </head>
   <body>
-<script>
+<script type = "text/javascript">
   //For LOOP
 for(var i=0; i<=10; ++i){
   console.log(i);
@@ -24,6 +24,21 @@ do{
   x *= 5;
   console.log(x);
 }while(x<100)
-    </script>
+ //For...in LOOP
+  var cars = ['jaguar', 'Tesla', 'BMW', ' Audi'];
+            for(const i in cars){
+               document.write(i +" => " + carss[i]);   
+               document.write("</br>");
+            }
+            document.write("</br>");
+
+// For...Of LOOP
+var fruits = ['apple', 'grapes', 'orange', 'banana'];
+            for(const i of fruits){
+               document.write(i);   
+               document.write("</br>");
+            }
+            document.write("</br>");
+ </script>
     </body>
 </html>
